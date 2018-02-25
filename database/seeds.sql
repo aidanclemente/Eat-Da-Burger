df5b91fe06a2ccb63d3ca370d36eb17310730c7e
aidanclemente <aidanclemente@gmail.com>
2018-02-25 08:53:21 -0500

    set up files

---
+++ database/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Spicy Chicken Burger", 0);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Cheese Burger", 0);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Bacon Blue Burger", 0);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Kids Cheese Burger", 1);
