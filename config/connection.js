c03559cc5cc9dbce9a4feb917d32d7c7f50fea68
aidanclemente <aidanclemente@gmail.com>
2018-02-28 11:50:10 -0500

    working on style

--- config/connection.js
+++ config/connection.js
@@ -6,6 +6,7 @@ var connection;
 if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
+  connection =
     mysql.createConnection({
     port: 3306,
     host: "localhost",
